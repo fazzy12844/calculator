b12ec6ae956eee1441735e94b8ad90c5244eb080
fazzy12844 <fazzyfaiz94@gmail.com>
2025-05-10 12:42:10 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,153 @@
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+        this.previousOperandTextElement = previousOperandTextElement;
+        this.currentOperandTextElement = currentOperandTextElement;
+        this.clear();
+    }
+
+    clear() {
+        this.currentOperand = '0';
+        this.previousOperand = '';
+        this.operation = undefined;
+    }
+
+    delete() {
+        this.currentOperand = this.currentOperand.toString().slice(0, -1);
+        if (this.currentOperand === '') {
+            this.currentOperand = '0';
+        }
+    }
+
+    appendNumber(number) {
+        if (number === '.' && this.currentOperand.includes('.')) return;
+        if (this.currentOperand === '0' && number !== '.') {
+            this.currentOperand = number;
+        } else {
+            this.currentOperand = this.currentOperand.toString() + number.toString();
+        }
+    }
+
+    chooseOperation(operation) {
+        if (this.currentOperand === '') return;
+        if (this.previousOperand !== '') {
+            this.compute();
+        }
+        this.operation = operation;
+        this.previousOperand = this.currentOperand;
+        this.currentOperand = '';
+    }
+
+    compute() {
+        let computation;
+        const prev = parseFloat(this.previousOperand);
+        const current = parseFloat(this.currentOperand);
+        if (isNaN(prev) || isNaN(current)) return;
+
+        switch (this.operation) {
+            case '+':
+                computation = prev + current;
+                break;
+            case '-':
+                computation = prev - current;
+                break;
+            case '×':
+                computation = prev * current;
+                break;
+            case '÷':
+                computation = prev / current;
+                break;
+            default:
+                return;
+        }
+
+        this.currentOperand = computation.toString();
+        this.operation = undefined;
+        this.previousOperand = '';
+    }
+
+    updateDisplay() {
+        this.currentOperandTextElement.innerText = this.currentOperand;
+        if (this.operation != null) {
+            this.previousOperandTextElement.innerText = 
+                `${this.previousOperand} ${this.operation}`;
+        } else {
+            this.previousOperandTextElement.innerText = '';
+        }
+    }
+}
+
+const numberButtons = document.querySelectorAll('[id^="zero"], [id^="one"], [id^="two"], [id^="three"], [id^="four"], [id^="five"], [id^="six"], [id^="seven"], [id^="eight"], [id^="nine"], [id^="decimal"]');
+const operationButtons = document.querySelectorAll('[id^="add"], [id^="subtract"], [id^="multiply"], [id^="divide"]');
+const equalsButton = document.querySelector('[id^="equals"]');
+const deleteButton = document.querySelector('[id^="delete"]');
+const allClearButton = document.querySelector('[id^="clear"]');
+const previousOperandTextElement = document.querySelector('[id^="previous-operand"]');
+const currentOperandTextElement = document.querySelector('[id^="current-operand"]');
+
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement);
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.innerText);
+        calculator.updateDisplay();
+    });
+});
+
+operationButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.chooseOperation(button.innerText);
+        calculator.updateDisplay();
+    });
+});
+
+equalsButton.addEventListener('click', button => {
+    calculator.compute();
+    calculator.updateDisplay();
+});
+
+allClearButton.addEventListener('click', button => {
+    calculator.clear();
+    calculator.updateDisplay();
+});
+
+deleteButton.addEventListener('click', button => {
+    calculator.delete();
+    calculator.updateDisplay();
+});
+
+// Keyboard support
+document.addEventListener('keydown', function(event) {
+    let patternForNumbers = /[0-9]/g;
+    let patternForOperators = /[+\-*\/]/g;
+    
+    if (event.key.match(patternForNumbers)) {
+        event.preventDefault();
+        calculator.appendNumber(event.key);
+        calculator.updateDisplay();
+    }
+    if (event.key === '.') {
+        event.preventDefault();
+        calculator.appendNumber(event.key);
+        calculator.updateDisplay();
+    }
+    if (event.key.match(patternForOperators)) {
+        event.preventDefault();
+        calculator.chooseOperation(event.key);
+        calculator.updateDisplay();
+    }
+    if (event.key === 'Enter' || event.key === '=') {
+        event.preventDefault();
+        calculator.compute();
+        calculator.updateDisplay();
+    }
+    if (event.key === "Backspace") {
+        event.preventDefault();
+        calculator.delete();
+        calculator.updateDisplay();
+    }
+    if (event.key == 'Delete') {
+        event.preventDefault();
+        calculator.clear();
+        calculator.updateDisplay();
+    }
+});
